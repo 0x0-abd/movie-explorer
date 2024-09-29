b5fcfc9fb7fc5e3667e010e06c7754ea4ef7a91c
nightdesert <87409279+nightdesert@users.noreply.github.com>
2024-09-29 16:48:36 +0530

    Added trailer

--- src/app/movies/[movieId]/page.tsx
+++ src/app/movies/[movieId]/page.tsx
@@ -14,6 +14,7 @@ export default function Movies({ params }: {
 }) {
 
     const [movieData, setMovieData] = useState<any>()
+    const [trailers, setTrailers] = useState<any[]>([]);
 
     const formatGenres = (genres: { id: number; name: string }[]): string => {
         return genres.map((genre) => genre.name).join(", ");
@@ -31,6 +32,10 @@ export default function Movies({ params }: {
                 const response = await axios(`/search/id/${params.movieId}`);
                 setMovieData(response.data);
                 // console.log(response.data);
+                const videoResponse = await axios(`${params.movieId}/videos`)
+                // console.log(videos.data)
+                setTrailers(videoResponse.data);
+
             } catch (e) {
                 console.error(e);
             }
@@ -42,56 +47,92 @@ export default function Movies({ params }: {
 
     return (
         <main>
-            <nav className="sticky top-0 flex h-16 justify-between items-center gap-4 px-4 z-50 md:px-6 border-b-2 bg-background ">
-                <div className="text-2xl">
-                    <Link href="/" className="flex gap-4 md:gap-6">
-                        <Clapperboard className="text-primary" />
-                        <p>Movies</p>
-                    </Link>
-                </div>
-                <div className="flex gap-4">
-                    <Link href="/movies" className="text-primary content-center underline"><div>Explore</div></Link>
-                    <ModeToggle />
-                </div>
-
-            </nav >
-            <div className="flex flex-col w-full justify-around items-center" style={{ height: "calc(100% - 4rem)" }}>
-
-                {/* {movieData?.results?.length as number === 0 ? (
-                    <>No movie found</>
-                ) : (
-                    <></>
-                ) }
-                {movieData?.results && movieData?.results?.length as number > 0 && <HoverEffect items={movieData?.results}/>} */}
-
-                {movieData ? (
-                    <div className="md:flex md:flex-row-reverse mt-6" style={{ maxWidth: "1336px" }}>
-                        <div className="md:w-4/5 flex justify-around p-4">
-                            <Image
-                                alt="Movie_Poster_Image"
-                                className=" rounded-xl"
-                                src={`https://image.tmdb.org/t/p/w500${movieData?.poster_path}`}
-                                sizes="(min-width: 808px) 90vw, 40vw"
-                                width={400}
-                                height={500}
-                            />
-                        </div>
-                        <div className="flex flex-col text-center p-4 md:text-left gap-4 md:gap-8">
-                            <div>
-                                <h1 className="text-5xl font-bold text-primary">{movieData?.title}</h1>
-                                <h3 className="text-xl text-muted-foreground">{formatGenres(movieData?.genres)} <span className="float-right">{movieData?.release_date}</span></h3>
-                            </div>
-                            <p>{movieData?.overview}</p>
-                            <p>{formatRuntime(movieData.runtime)}</p>
-                        </div>
-
-                    </div>
-                ) : (
-                    <div className="text-3xl">
-                        Movie data not available
-                    </div>
-                )}
+      <nav className="sticky top-0 flex h-16 justify-between items-center gap-4 px-4 z-50 md:px-6 border-b-2 bg-background">
+        <div className="text-2xl">
+          <Link href="/" className="flex gap-4 md:gap-6">
+            <Clapperboard className="text-primary" />
+            <p>Movies</p>
+          </Link>
+        </div>
+        <div className="flex gap-4">
+          <Link href="/movies" className="text-primary content-center underline">
+            <div>Explore</div>
+          </Link>
+          <ModeToggle />
+        </div>
+      </nav>
+      <div
+        className="flex flex-col w-full justify-around items-center"
+        style={{ height: "calc(100% - 4rem)" }}
+      >
+        {movieData ? (
+          <div
+            className="md:flex md:flex-row-reverse mt-6"
+            style={{ maxWidth: "1336px" }}
+          >
+            <div className="md:w-4/5 flex justify-around p-4">
+              <Image
+                alt="Movie_Poster_Image"
+                className="rounded-xl"
+                src={`https://image.tmdb.org/t/p/w500${movieData?.poster_path}`}
+                sizes="(min-width: 808px) 90vw, 40vw"
+                width={400}
+                height={500}
+              />
             </div>
-        </main >
+            <div className="flex flex-col text-center p-4 md:text-left gap-2 md:gap-4">
+              <div>
+                <h1 className="text-5xl font-bold text-primary">
+                  {movieData?.title}
+                </h1>
+                <h3 className="text-xl text-muted-foreground">
+                  {formatGenres(movieData?.genres)}{" "}
+                  <span className="float-right">{movieData?.release_date}</span>
+                </h3>
+              </div>
+              <p>{movieData?.overview}</p>
+              <p>{formatRuntime(movieData.runtime)}</p>
+              {trailers.length > 0 && (
+                <iframe
+                  width="840"
+                  height="472.5"
+                  src={`https://www.youtube.com/embed/${trailers[0].key}`}
+                  title={trailers[0].name}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="rounded-lg mt-4"
+                ></iframe>
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className="text-3xl">Movie data not available</div>
+        )}
+
+        {/* Trailers Section */}
+        <div className="mt-8 w-full p-4">
+          <h2 className="text-3xl font-bold text-center">Other Trailers</h2>
+          <div className="flex flex-col md:flex-row justify-center items-center gap-4 mt-4">
+            {trailers.length > 0 ? (
+              trailers.slice(1).map((trailer) => (
+                <iframe
+                  key={trailer.id}
+                  width="560"
+                  height="315"
+                  src={`https://www.youtube.com/embed/${trailer.key}`}
+                  title={trailer.name}
+                  loading="lazy"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="rounded-lg"
+                ></iframe>
+              ))
+            ) : (
+              <p>No trailers available</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,21 @@ import { motion } from "framer-motion";
 import { FlipWords } from "@/components/ui/flip-words"
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Input } from "@/components/ui/input";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import Link from "next/link"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "@/lib/axios";
+import Image from "next/image";
 
 const formSchema = z.object({
   searchQuery: z.string()
@@ -18,7 +27,8 @@ const formSchema = z.object({
 
 export default function Home() {
   const router = useRouter()
-  const [query, setQuery] = useState<string>()
+  const [query, setQuery] = useState<string>();
+  const [carouselData, setCarouselData] = useState<any[]>();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -27,6 +37,19 @@ export default function Home() {
     },
   })
 
+  useEffect(()=>  {
+    const getCarouselData = async() => {
+      try {
+        const response = await axios(`/search/`)
+        console.log(response.data)
+        setCarouselData(response.data.results)
+        console.log(carouselData)
+      } catch(e) {
+        console.log(e)
+      }
+    }
+    getCarouselData();
+  }, [])
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
 
@@ -53,7 +76,15 @@ export default function Home() {
 
       </nav>
       <div className="flex flex-col w-full h-full text-center justify-around" style={{ height: "calc(100% - 4rem)" }}>
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3"><Image alt="something" src="https://image.tmdb.org/t/p/w500/fqv8v6AycXKsivp1T5yKtLbGXce.jpg" width={500} height={300} /></CarouselItem>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3"><Image alt="something" src="https://image.tmdb.org/t/p/w500/z121dSTR7PY9KxKuvwiIFSYW8cf.jpg" width={500} height={300} /></CarouselItem>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3"><Image alt="something" src="https://image.tmdb.org/t/p/w500/3TNSoa0UHGEzEz5ndXGjJVKo8RJ.jpg" width={500} height={300} /></CarouselItem>
+          </CarouselContent>
+        </Carousel>
         <div className="flex flex-col py-4 px-4">
+
           <div className="text-3xl md:text-7xl font-bold dark:text-white w-full">Want <FlipWords words={genreNames} className=" font-bold" /></div>
           <motion.div
             initial={{ opacity: 0.0, y: 40 }}
@@ -65,6 +96,7 @@ export default function Home() {
             }}
             className="relative flex flex-col gap-4 items-center justify-center px-4"
           >
+
             <div className="font-extralight text-xl md:text-4xl dark:text-neutral-200 py-4">
               We have it all.
             </div>
